d70df42650f20003a781cc14359a1fc8b5490f1a
Ghassan-Dib <gid01@mail.aub.edu>
2021-12-09 17:58:48 +0200

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    
+    "parser": "@babel/eslint-parser",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true,
+            "modules": true,
+            "experimentalObjectRestSpread": true
+        }
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    },
+    "settings": {
+      "react": {
+        "version": "detect"
+      }
+    },
+    "extends": ["plugin:jest/recommended"],
+    "plugins": ["jest"]
+    
+}
