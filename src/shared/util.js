de52c0fb2d77a53e2afc0fd760c317fbeb46d685
Ghassan-Dib <gid01@mail.aub.edu>
2021-12-09 18:19:53 +0200

    edit

--- src/shared/util.js
+++ src/shared/util.js
@@ -1,4 +1,3 @@
 // @flow
 
-/* eslint-disable import/prefer-default-export */
-export const isProd = process.env.NODE_ENV === 'production';
+export default const isProd = process.env.NODE_ENV === 'production';
