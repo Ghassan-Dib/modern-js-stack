1bc543b55014874cc0a8139a1ab00f561b012972
Ghassan-Dib <gid01@mail.aub.edu>
2021-12-09 17:56:41 +0200

    updates

--- src/shared/routes.js
+++ src/shared/routes.js
@@ -1,12 +1,12 @@
 // @flow
 
-export default const HOME_PAGE_ROUTE = '/';
-export default const HELLO_PAGE_ROUTE = '/hello';
-export default const HELLO_ASYNC_PAGE_ROUTE = '/hello-async';
-export default const NOT_FOUND_DEMO_PAGE_ROUTE = '/404';
+export const HOME_PAGE_ROUTE = '/';
+export const HELLO_PAGE_ROUTE = '/hello';
+export const HELLO_ASYNC_PAGE_ROUTE = '/hello-async';
+export const NOT_FOUND_DEMO_PAGE_ROUTE = '/404';
 
 // eslint-disable-next-line import/prefer-default-export
-export default const helloEndpointRoute: any = (num: number) => {
+export const helloEndpointRoute: any = (num: number) => {
     console.log(`number = ${num}`);
     return `/ajax/hello/${num || ':num'}`;
 };
